e050cb07e22f04eb6e52dc8718bd656d3034c5b2
Patrick <palbino843@gmail.com>
2024-09-11 18:13:33 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -55,11 +55,30 @@ const start = () => {
 
 start()*/
 
-const start = () => {
+/*const start = () => {
    let count = 0
    while (count < 10) {
          console.log(count)
          count++     // OU count = count + 1  
+   }
+}
+
+start()*/
+
+const start = () => {
+   
+   while (true) {
+      let opcao = "cadastrar"
+      switch (opcao) {
+         case "cadastrar":
+            console.log("vamos cadastrar")
+            break
+         case "listar":
+            console.log("vamos listar")
+            break
+         case "sair":
+            return
+      }
    }
 }
 
